c000f8cfdb8a080aad28563ae1421b8b74ba2021
rahulshamkuwar <rahulshamkuwar20@gmail.com>
2023-09-30 20:02:10 -0600

    Update install husky script so that it reads .env

--- install-husky.ts
+++ install-husky.ts
@@ -1,10 +1,11 @@
 const { execSync } = require("child_process");
+require("dotenv").config();
 
 const nodeEnv = process.env.NODE_ENV;
 
 if (nodeEnv === "development") {
   console.log("Installing Husky hooks...");
-  execSync("husky install", { stdio: "inherit" });
+  execSync("npx husky install", { stdio: "inherit" });
 } else {
   console.log('NODE_ENV is not set to "development"; skipping Husky install.');
 }
