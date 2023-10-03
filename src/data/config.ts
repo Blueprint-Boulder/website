8534c8d08d12bb45617d450dbe309339caed2a6f
rahulshamkuwar <rahulshamkuwar20@gmail.com>
2023-10-02 20:38:11 -0600

    Add all components. Have yet to finish testing all of them.

---
+++ src/data/config.ts
@@ -0,0 +1,34 @@
+module.exports = {
+  defaultTitle: "Blueprint Boulder",
+  logo: "https://blueprintboulder.org/favicon/favicon-512.png",
+  author: "Blueprint Boulder Team",
+  url: "https://blueprintboulder.org",
+  legalName: "Michal Bodzianowski",
+  defaultDescription:
+    "We're Blueprint Boulder and we make tech happen for social good!",
+  socialLinks: {
+    // twitter: 'http://www.twitter.com/smakosh',
+    github: "https://github.com/blueprintboulder",
+    linkedin: "https://www.linkedin.com/company/blueprint-boulder/",
+    instagram: "https://www.instagram.com/blueprintboulder/",
+    youtube: "https://www.youtube.com/channel/UCfD84rDxS70h5P9zomNo-7A",
+  },
+  googleAnalyticsID: "G-SPEL6XH047",
+  themeColor: "#0078e8",
+  backgroundColor: "#0078e8",
+  social: {
+    facebook: "appId",
+    twitter: "@blueprintboulder",
+  },
+  address: {
+    city: "Boulder",
+    region: "Colorado",
+    country: "United States",
+    zipCode: "80310",
+  },
+  contact: {
+    email: "blueprintboulder@colorado.edu",
+    phone: "phone number",
+  },
+  foundingDate: "2020",
+};
