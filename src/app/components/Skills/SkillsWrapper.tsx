fd8f7cb62dd8b8404b32850e1f03200d5c58d6ca
Trish Le <trle5720@colorado.edu>
2023-11-05 17:41:43 -0700

    make PR changes for adding skills components

--- src/app/components/Skills/SkillsWrapper.tsx
+++ src/app/components/Skills/SkillsWrapper.tsx
@@ -7,7 +7,8 @@ interface SkillsWrapperProps{
 
 const SkillsWrapper: React.FC<SkillsWrapperProps> = ({children, as:AsComponent="div"}) => {
     return(
-        <AsComponent className="p-16 flex items-center justify-between max-[960px]:flex-col max-w-7xl my-0 mx-auto w-full sm:w-[9/10] lg:w-[4/5]" >{children}</AsComponent>
+        <AsComponent className="py-16 px-28 flex items-center justify-between max-lg:flex-col" 
+        >{children}</AsComponent>
     );
 };
 
--- src/app/components/Skills/Wrapper.tsx
+++ src/app/components/Skills/Wrapper.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Image from 'next/image';
 import detailsIllustration from 'public/illustrations/details.svg';
+import { relative } from 'path';
 
 interface WrapperProps{
     children: React.ReactNode;
@@ -10,9 +12,20 @@ interface WrapperProps{
 const Wrapper: React.FC<WrapperProps> = ({children,as:AsComponent="div",...props})=> {
     return(
         <AsComponent
-            className = "bg-contain bg-left-top bg-no-repeat" {...props}
-            style={{backgroundImage: `url(${detailsIllustration.src})`}}
-        >{children}</AsComponent>
+            className = "bg-contain bg-left-top bg-no-repeat absolute " {...props}
+        >
+            <Image
+                className="skills-bg" 
+                alt="details"
+                src={detailsIllustration}
+                quality={100}
+                width={0}
+                height={0}
+                style={{zIndex:-1,objectFit:'fill',position:'absolute'}} // optional
+            />
+            
+            <div>{children}</div>
+        </AsComponent>
     );
 };
 
