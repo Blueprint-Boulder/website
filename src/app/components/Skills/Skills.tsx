8f9c1e398f6143740c82cc7f7466e60aafb849f1
Trish Le <trle5720@colorado.edu>
2023-11-05 17:41:43 -0700

    make PR changes for adding skills components

--- src/app/components/Skills/Skills.tsx
+++ src/app/components/Skills/Skills.tsx
@@ -11,17 +11,16 @@ import Button from '../Buttons/Button';
 export default function Skills(){
     return(
         <Wrapper as="div">
-            <SkillsWrapper>
+            <SkillsWrapper as="div">
                 <Thumbnail>
                     <Image src={dev} alt="Blueprint Boulder"/>
                 </Thumbnail>
 
-                <Details 
-                    header="Are you a CU affiliate?" 
-                    para="Blueprint Boulder is open to CU students of any skill level
+                <Details>
+                    <h1>Are you a CU affiliate?</h1>
+                    <p>Blueprint Boulder is open to CU students of any skill level
                     who are interested in design, development, or establishing relationships with our non-profit partners.
-                    Click below to sign up for our interest form, and we'll be in contact with you soon!"
-                >
+                    Click below to sign up for our interest form, and we'll be in contact with you soon!</p>
                     <a target="_blank" rel="noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSf_rvzbG-OqXEl1x7iKJWV5mv3Nnr9Zp07WFrFu0If-hSjePQ/viewform">
                         <Button secondary={true}>
                             Interest Form 2023-2024
@@ -29,13 +28,12 @@ export default function Skills(){
                     </a>
                 </Details>
 
-                <Details 
-                    header="Are you a Non-profit?"
-                    para="We accept meeting requests from nonprofits in January and February
+                <Details>
+                    <h1>Are you a Non-profit?</h1>
+                    <p>We accept meeting requests from nonprofits in January and February
                     for projects during the following academic year. Our team will collaborate
                     with you to plan your software project, and finalists will have their project
-                    built starting in the fall semester, with completion by the end of the spring semester"
-                >
+                    built starting in the fall semester, with completion by the end of the spring semester</p>
                     <a target="_blank" rel="noreferrer" href="">
                         <Button secondary={true}>
                             Work With Us!
