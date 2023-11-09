c53c013dfe28cf88a21bbb9ab28c6f073ff3089c
rahulshamkuwar <rahulshamkuwar20@gmail.com>
2023-11-09 11:17:26 -0700

    Finish some parts of the ticket

--- src/app/components/Contact/Details.tsx
+++ src/app/components/Contact/Details.tsx
@@ -6,7 +6,7 @@ interface DetailsProps {
 
 export const Details: React.FC<DetailsProps> = ({ children }) => {
   return (
-    <div className="flex-1 pr-8 lg:pr-unset lg:w-full lg:order-1 contact-details ">
+    <div className="flex-1 pr-8 max-lg:pr-unset max-lg:w-full max-lg:order-1 contact-details ">
       {children}
     </div>
   );
